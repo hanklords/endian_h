c346aac3712fb1a6e2065fa83248b8914ad6fe6f
Maël Clérambault <mael@clerambault.fr>
2014-03-12 10:42:09 +0100

    Remove sha1

--- endian_h.rb
+++ endian_h.rb
@@ -3,7 +3,6 @@ require 'formula'
 class EndianH < Formula
   homepage 'https://github.com/hanklords/endian_h'
   url 'https://github.com/hanklords/endian_h.git'
-  sha1 'fb3f00802347ee53901632d2866c5a8e394a6229'
   version "1"
 
   def install
